93c08bcccb50db67e6ade4cd81eff72c150e5208
bannysha157 <98574529+bannysha157@users.noreply.github.com>
2023-02-28 15:03:07 +0530

    Basics_automate_ boring_stuff

--- basics.c
+++ basics.c
@@ -8,9 +8,56 @@
 +
 5
 
-
+sln:
 The operators are +, -, *, and /.
 The values are 'hello', -88.8, and 5.
 
+  
+2. Which of the following is a variable, and which is a string?
+spam
+'spam'
 
+sln:
+The string is 'spam'.Strings always start and end with quotes.
+the variable is spam. 
+  
+  
+3)Name three data types.
+sln:
+The three data types introduced in this chapter are integers, floating-point numbers, and strings.
 
+4)What is an expression made up of? What do all expressions do?
+  sln:An expression is a combination of values and operators.
+    All expressions evaluate (that is, reduce) to a single value.
+
+ 5) This chapter introduced assignment statements, like spam = 10.
+    What is the difference between an expression and a statement?  
+    
+    sln:An expression evaluates to a single value. A statement does not.
+
+6. What does the variable bacon contain after the following code runs?
+bacon = 20
+bacon + 1
+ 
+sln:   The bacon variable is set to 20.
+The bacon + 1 expression does not reassign the value in bacon (that would need an assignment statement: bacon = bacon + 1).
+  
+ 7)What should the following two expressions evaluate to?
+'spam' + 'spamspam'
+'spam' * 3
+  
+  sln: Both expressions evaluate to the string 'spamspamspam'.
+ 
+8). Why is eggs a valid variable name while 100 is invalid?
+  sln: Variable names cannot begin with a number.
+   
+ 9)What three functions can be used to get the integer, floating-point number, or string version of a value?
+    sln:The int(), float(), and str() functions will evaluate to the integer, floating-point number, and string versions of the value passed to them.
+      
+10)Why does this expression cause an error? How can you fix it?
+'I have eaten ' + 99 + ' burritos.'
+      
+ sln:The expression causes an error because 99 is an integer, and only strings can be concatenated to other strings with the + operator. 
+   The correct way is I have eaten ' + str(99) + ' burritos.'.
+   
+   
